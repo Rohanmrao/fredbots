85674bb9ab7b55fa0b85c3a051ea9778def5e1a1
Rohanmrao <rohanmaheshrao10@gmail.com>
2023-06-23 15:16:56 +0530

    Removed unwanted data at output + rearranged data

--- src/scripts/atom_move.py
+++ src/scripts/atom_move.py
@@ -6,7 +6,8 @@ import gym
 from gym import spaces
 import rospy
 from std_srvs.srv import Empty
-from geometry_msgs.msg import Twist, Pose
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 from math import pow, atan2, sqrt
 import numpy as np
 
@@ -16,7 +17,7 @@ class AtomEnv(gym.Env):
 
         rospy.init_node('atom_move', anonymous=True)
         self.velocity_publisher = rospy.Publisher('/atom/cmd_vel', Twist, queue_size=10)
-        self.pose_subscriber = rospy.Subscriber('/atom/odom', Pose, self.update_pose)
+        self.pose_subscriber = rospy.Subscriber('/atom/odom', Odometry, self.update_pose)
         # self.reset_proxy = rospy.ServiceProxy('/reset', Empty)
 
         self.action_space = spaces.Discrete(4)  # Up, Down, Left, Right
@@ -26,8 +27,8 @@ class AtomEnv(gym.Env):
 
         self.goal_x = 3   # destination coordinates
         self.goal_y = 3
-        self.position_x = 5.544445  # init coordinates
-        self.position_y = 5.544445
+        # self.position_x = 0  # init coordinates
+        # self.position_y = 0
 
         self.episodes = 5
         self.learning_rate = 0.5
@@ -36,11 +37,11 @@ class AtomEnv(gym.Env):
 
         self.q_table = np.zeros((10, 10, 4))
 
-    # def reset(self):
-    #     self.reset_proxy()  # Reset the turtlesim simulation
-    #     # self.position_x = 0
-    #     # self.position_y = 0    # reset to init coordinates
-    #     return self.get_observation()
+    def reset(self):
+        # self.reset_proxy()  # Reset the turtlesim simulation
+        self.position_x = 0
+        self.position_y = 0    # reset to init coordinates
+        return self.get_observation()
 
     def step(self, action):
         self.move_atom(action)
@@ -67,8 +68,8 @@ class AtomEnv(gym.Env):
         self.velocity_publisher.publish(velocity_msg)
 
     def update_pose(self, data):
-        self.position_x = round(data.linear.x, 2)
-        self.position_y = round(data.linear.y, 2)
+        self.position_x = round(data.pose.pose.position.x, 2)
+        self.position_y = round(data.pose.pose.position.y, 2)
 
     def get_observation(self):
 
@@ -86,29 +87,27 @@ class AtomEnv(gym.Env):
 
     def q_learning(self):
         for episode in range(self.episodes):
-            # state = self.reset()
-            state = [self.goal_x, self.goal_y, self.position_x, self.position_y]
+            state = self.reset()
             done = False
 
             while not done:
-                print("Episode no. : ",episode)
+                print("\n\nEpisode no. : ",episode)
+                print("Overall State : ",state)
                 state_index = self.get_state_index(state)
-                print("start state ",state_index)
-                print("pos_x, pos_y ",self.position_x,self.position_y)
+                print("Current State ",state[2],state[3])
                 action = self.get_action(state_index)
-                print("action ",action)
+                print("Action ",action)
 
                 next_state, reward, done, _ = self.step(action)
                 next_state_index = self.get_state_index(next_state)
-                print("next start state ",next_state_index)
-                print("reward ", reward)
-
+                print("Next State ",next_state_index)
+                print("Reward ", reward)
                 current_q_value = self.q_table[state_index][action]
-                print("old_q_value ",current_q_value)
+                print("Old_Q_Value ",current_q_value)
                 max_q_value = np.max(self.q_table[next_state_index])
-                print("max q value of present state ", max_q_value)
+                print("Max Q Value of Present State ", max_q_value)
                 new_q_value = (1 - self.learning_rate) * current_q_value + self.learning_rate * (reward + self.discount_factor * max_q_value)
-                print("new_q_value of present state ",new_q_value)
+                print("New_Q_Value of Present State ",new_q_value)
 
                 self.q_table[state_index][action] = new_q_value
 
