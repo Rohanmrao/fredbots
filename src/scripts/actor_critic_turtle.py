df273b622cb1cdb409783e95648a10e197995d24
Amulya Marali <maraliamulya@gmail.com>
2023-06-28 16:23:30 +0530

    only reset not working

--- src/scripts/actor_critic_turtle.py
+++ src/scripts/actor_critic_turtle.py
@@ -1,31 +1,15 @@
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
+from std_srvs.srv import Empty
 import math
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+# from sklearn.preprocessing import OneHotEncoder
 
 
-# Actor-Critic model
-# class ActorCriticModel(keras.Model):
-#     def __init__(self, num_actions):
-#         super(ActorCriticModel, self).__init__()
-#         self.dense1 = layers.Dense(32, activation='relu')
-#         self.dense3 = layers.Dense(32, activation='relu')
-#         self.policy_logits = layers.Dense(num_actions)
-#         self.dense2 = layers.Dense(32, activation='relu')
-#         self.dense4 = layers.Dense(32, activation='relu')
-#         self.values = layers.Dense(1)
-
-#     def call(self, inputs):  # call is like a forward function 
-#         x = self.dense1(inputs)
-#         logits = self.policy_logits(x)
-#         v = self.dense2(inputs)
-#         values = self.values(v)
-#         return logits, values
-    
 
 
 # Actor model
@@ -78,6 +62,7 @@ class TurtlesimActorCriticAgent:
         self.model = ActorCriticModel(num_actions)
         self.optimizer = keras.optimizers.Adam(learning_rate=0.01)
         self.huber_loss = keras.losses.Huber()
+        self.loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
         # Huber loss is a type of loss function commonly used in regression tasks, including reinforcement learning.
 
     def get_action(self, state):
@@ -85,6 +70,7 @@ class TurtlesimActorCriticAgent:
         print("shape of state to get_action fn: ", state.shape)
         logits, _ = self.model(state) #state is the input
         # print("logits: ", logits)
+        # print("self.model.output[0] for actions: ",self.model.output[0])
         action_probabilities = tf.nn.softmax(logits)
         # print("action_probs b4 converting to numpy: ",action_probabilities)
         action_probabilities = action_probabilities[0].numpy()  # Convert to NumPy array
@@ -95,13 +81,34 @@ class TurtlesimActorCriticAgent:
 
     def train(self, states, actions, discounted_rewards):
         with tf.GradientTape() as tape:  # tf.GradientTape() is a TensorFlow API that enables automatic differentiation. 
+            # states = tf.convert_to_tensor([states], dtype=tf.float32)
             print("shape of states to train fn: ", states.shape)
-            logits, values = self.model(states)
-            advantage = discounted_rewards - values
 
-            actions_one_hot = tf.one_hot(actions, depth=len(self.model.output[0]))
-            policy_loss = self.huber_loss(actions_one_hot, logits, from_logits=True)
-            value_loss = self.huber_loss(discounted_rewards, values)
+            # states = self.state
+            # states = tf.convert_to_tensor([states], dtype=tf.float32)
+            
+            # print("self.model.output[0]: ",self.model.output[0])
+
+            logits, values = self.model(states)
+            print("logits: ",logits)
+            advantage = discounted_rewards - values
+            print("ACTIONS SHAPE BEFORE ONE HOT")
+            print(actions.shape)
+            # print("self.model.output[0]",self.model.output[0])
+            # actions_one_hot = tf.one_hot(actions, depth=self.model.output[0])
+            # print(actions_one_hot,"%%"*5)
+            # print("self.model.output[0]: ",self.model.output[0])
+
+            # actions_one_hot = tf.one_hot(actions, depth=5)
+            # policy_loss = self.huber_loss(actions_one_hot, logits, from_logits=True)
+            # value_loss = self.huber_loss(discounted_rewards, values)
+
+            actions_one_hot = tf.one_hot(actions, depth=len(logits[0]))
+            policy_loss = self.loss_fn(actions_one_hot, logits)
+
+            discounted_rewards = tf.reshape(discounted_rewards, values.shape)
+
+            value_loss = self.loss_fn(discounted_rewards, values)
 
             total_loss = policy_loss + value_loss
 
@@ -131,6 +138,7 @@ class TurtleBot3Controller:
         rospy.init_node('turtlebot_controller', anonymous=True)
         self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
         self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.pose_callback)
+        self.reset_proxy = rospy.ServiceProxy('/reset', Empty)
         self.rate = rospy.Rate(10)  # 10hz
 
     def pose_callback(self, data):
@@ -142,6 +150,17 @@ class TurtleBot3Controller:
     def set_target_position(self, target_x, target_y):
         self.target_x = target_x
         self.target_y = target_y
+
+    def reset(self):
+        self.reset_proxy()  # Reset the turtlesim simulation
+        self.current_x = 5.544445  # init coordinates
+        self.current_y = 5.544445
+        print("\ntest\n")
+        print(self.current_x, self.current_y)
+        # return self.get_observation()
+    
+
+
 
     # def move_turtle(self):
     #     while not rospy.is_shutdown():
@@ -189,6 +208,8 @@ class TurtleBot3Controller:
 
     def train_agent(self, num_episodes):
         for episode in range(num_episodes):
+
+            self.reset()
             # self.set_target_position(np.random.uniform(0, 10), np.random.uniform(0, 10))
             self.set_target_position(4,4)
             episode_reward = 0
@@ -215,6 +236,11 @@ class TurtleBot3Controller:
                     print("target_y", self.target_y)
                     distance_to_target = self.euclidean_distance(current_x, current_y, self.target_x, self.target_y)
                     print("distance to target: ", distance_to_target)
+
+                    distance_to_bound = self.euclidean_distance(current_x, current_y, 5.544445 , 5.544445 )
+                    print("distance_to_bound: ", distance_to_bound)
+                    if distance_to_bound >2.5: # more than 2.5 radius from start position
+                        break
                     if distance_to_target < 0.5:  # Reached target
                         break
 
@@ -258,7 +284,7 @@ class TurtleBot3Controller:
                     episode_discounted_rewards.append(reward)
 
                 self.rate.sleep()
-
+            # print(episode_states)
             print("i came out")
             # Stop the turtle
             vel_msg = Twist()
@@ -289,6 +315,7 @@ class TurtleBot3Controller:
 
             # Train agent
             self.agent.train(episode_states, episode_actions, discounted_rewards)
+           
 
             print(f"Episode {episode + 1}: Reward = {episode_reward}")
 
