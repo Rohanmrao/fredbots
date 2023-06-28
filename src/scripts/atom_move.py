8a4619cf23434e57e494731842a834256b164bd4
Ashvin Bhat <ashvinbhat14@gmail.com>
2023-06-28 16:55:46 +0530

    fix

--- src/scripts/atom_move.py
+++ src/scripts/atom_move.py
@@ -7,9 +7,11 @@ from std_srvs.srv import Empty
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from math import pow, atan2, sqrt
-from gazebo_msgs.msg import ModelState 
-from gazebo_msgs.srv import SetModelState
 import numpy as np
+from gazebo_msgs.srv import SetModelState
+from gazebo_msgs.msg import ModelState
+
+
 
 class AtomEnv(gym.Env):
     def __init__(self):
@@ -19,21 +21,33 @@ class AtomEnv(gym.Env):
         self.pose_subscriber = rospy.Subscriber('/atom/odom', Odometry, self.update_pose)
         # self.reset_proxy = rospy.ServiceProxy('/reset', Empty)
 
+        self.set_model_state_proxy = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+
+        self.model_state_msg = ModelState()
+        self.model_state_msg.model_name = 'atom_bot'
+        self.model_state_msg.pose.position.x = 0.0
+        self.model_state_msg.pose.position.y = 0.0
+        self.model_state_msg.pose.position.z = 0.0
+        self.model_state_msg.pose.orientation.x = 0.0
+        self.model_state_msg.pose.orientation.y = 0.0
+        self.model_state_msg.pose.orientation.z = 0.0
+        self.model_state_msg.pose.orientation.w = 1.0
+
         self.action_space = spaces.Discrete(4)  # Up, Down, Left, Right
         self.observation_space = spaces.Box(low=0, high=10, shape=(4,))
         self.rate = rospy.Rate(10)
-
-        self.goal_x = 2   # destination coordinates
-        self.goal_y = 2
+        self.goal_x = 3   # destination coordinates
+        self.goal_y = 3
+        # self.position_x = 0  # init coordinates
+        # self.position_y = 0
         self.position_x = 0  # init coordinates
         self.position_y = 0
         self.episodes = 5
         self.learning_rate = 0.5
         self.discount_factor = 0.99
-        self.epsilon = 0.9
+        self.epsilon = 0.2
         self.q_table = np.zeros((10, 10, 4))
-        self.state_msg = ModelState()
-   
+    
     def update_pose(self, data):
         self.position_x = round(data.pose.pose.position.x, 2)
         self.position_y = round(data.pose.pose.position.y, 2)
@@ -42,27 +56,8 @@ class AtomEnv(gym.Env):
         # self.reset_proxy()  # Reset the turtlesim simulation
         self.position_x = 0
         self.position_y = 0    # reset to init coordinates
-        self.set_robot_coordinates_to_init()
         return self.get_observation()
     
-    def set_robot_coordinates_to_init(self):
-
-        print("\n RESET")
-        self.state_msg.model_name = 'atom'
-        self.state_msg.pose.position.x = 0
-        self.state_msg.pose.position.y = 0
-        self.state_msg.pose.position.z = 0
-        self.state_msg.pose.orientation.x = 0
-        self.state_msg.pose.orientation.y = 0
-        self.state_msg.pose.orientation.z = 0
-        self.state_msg.pose.orientation.w = 0
-        rospy.wait_for_service('/gazebo/set_model_state')
-        try:
-            set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
-            resp = set_state(self.state_msg)
-        except rospy.ServiceException:
-            print("Service call failed")
-
     def step(self, action):
         self.move_atom(action)
         self.rate.sleep()
@@ -70,17 +65,20 @@ class AtomEnv(gym.Env):
         reward = self.get_reward()
         done = self.is_done()
         return obs, reward, done, {}
-    
 
     def move_atom(self, action):
         if action == 0:  # Up
-            self.move(11.0, 0.0)
+            self.move(5.0, 0.0)
+            self.move(40.0, 0.0)
         elif action == 1:  # Down
-            self.move(-11.0, 0.0)
+            self.move(-5.0, 0.0)
+            self.move(-40.0, 0.0)
         elif action == 2:  # Left
-            self.move(0.0, -11.0)
+            self.move(0.0, 5.0)
+            self.move(0.0, 40.0)
         elif action == 3:  # Right
-            self.move(0.0, -11.0)
+            self.move(0.0, -5.0)
+            self.move(0.0, -40.0)
 
     def move(self, linear_vel, angular_vel):
         velocity_msg = Twist()
@@ -97,14 +95,13 @@ class AtomEnv(gym.Env):
         return reward
     
     def is_done(self):
+        dist_bound = sqrt(pow(0 - self.position_x, 2) + pow(0 - self.position_y, 2))
+        print("dist_bound ",dist_bound)
+        if dist_bound > 2:
+            # response = self.set_model_state_proxy(self.model_state_msg)
+            print("reset DONE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         distance_threshold = 0.5
         distance_to_goal = sqrt(pow(self.goal_x - self.position_x, 2) + pow(self.goal_y - self.position_y, 2))
-
-        distance_to_bound = sqrt(pow(self.position_x - 0, 2) + pow(self.position_y - 0, 2))
-
-        if distance_to_bound >= 9:
-            self.set_robot_coordinates_to_init()
-
         return distance_to_goal < distance_threshold
     
     def q_learning(self):
@@ -122,6 +119,7 @@ class AtomEnv(gym.Env):
                 next_state, reward, done, _ = self.step(action)
                 print("next state ",next_state[2],next_state[3])
                 next_state_index = self.get_state_index(next_state)
+                # print("Next State ",next_state_index)
                 print("Reward ", reward)
                 current_q_value = self.q_table[state_index][action]
                 print("Old_Q_Value ",current_q_value)
@@ -133,7 +131,7 @@ class AtomEnv(gym.Env):
                 state = next_state
             print(f"Episode: {episode + 1} completed.")
 
-    def get_state_index(self, state):      
+    def get_state_index(self, state):        # return np.argmax(self.q_table[x][y])
         x = int(state[2])  # position_x
         y = int(state[3])  # position_y
         return x, y
