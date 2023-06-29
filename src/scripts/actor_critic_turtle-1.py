e0500a2744319c4d137a7c197f26d5d8f2aab0cf
Amulya Marali <maraliamulya@gmail.com>
2023-06-29 20:24:40 +0530

    a2c atom reset works

--- src/scripts/actor_critic_turtle-1.py
+++ src/scripts/actor_critic_turtle-1.py
@@ -142,13 +142,7 @@ class TurtleBot3Controller:
         self.rate = rospy.Rate(10)  # 10hz
 
     def pose_callback(self, data):
-        self.state = [
-            data.x,
-            data.y,
-            data.theta,
-            data.linear_velocity,
-            data.angular_velocity,
-        ]
+        self.state = [data.x, data.y, data.theta, data.linear_velocity, data.angular_velocity,]
 
     def euclidean_distance(self, x1, y1, x2, y2):
         return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
