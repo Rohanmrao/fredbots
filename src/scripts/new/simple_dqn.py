aeddf12a1441855a6ef49642edd14c2e39cd9c0e
Amulya Marali <maraliamulya@gmail.com>
2023-07-13 10:03:36 +0530

    simple dqn

---
+++ src/scripts/new/simple_dqn.py
@@ -0,0 +1,81 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
+# Define the Q-Network model
+def create_q_network(input_shape, action_space):
+    model = Sequential()
+    model.add(Dense(32, activation='relu', input_shape=input_shape))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(action_space, activation='linear'))
+    model.compile(loss='mse', optimizer=Adam(learning_rate=0.001))
+    return model
+
+# Define the DQN agent
+class DQNAgent:
+    def __init__(self, state_shape, action_space):
+        self.state_shape = state_shape
+        self.action_space = action_space
+        self.memory = []
+        self.gamma = 0.95  # Discount factor
+        self.epsilon = 1.0  # Exploration rate
+        self.epsilon_decay = 0.995  # Decay rate for exploration rate
+        self.epsilon_min = 0.01  # Minimum exploration rate
+        self.model = create_q_network(state_shape, action_space)
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.action_space)
+        q_values = self.model.predict(state)
+        return np.argmax(q_values[0])
+
+    def replay(self, batch_size):
+        if len(self.memory) < batch_size:
+            return
+        batch = np.random.choice(self.memory, batch_size, replace=False)
+        for state, action, reward, next_state, done in batch:
+            target = reward
+            if not done:
+                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+# Initialize the environment and agent
+state_shape = (4,)  # Example state shape, adjust according to your actual state representation
+action_space = 4  # Example action space size, adjust according to your actual actions
+agent = DQNAgent(state_shape, action_space)
+
+# Training loop
+num_episodes = 1000  # Set the number of training episodes
+batch_size = 32  # Set the batch size for replay
+for episode in range(num_episodes):
+    state = env.reset()
+    state = np.reshape(state, state_shape)
+    done = False
+    while not done:
+        action = agent.act(state)
+        next_state, reward, done, _ = env.step(action)
+        next_state = np.reshape(next_state, state_shape)
+        agent.remember(state, action, reward, next_state, done)
+        state = next_state
+
+        if len(agent.memory) > batch_size:
+            agent.replay(batch_size)
+
+# Use the trained agent to control the agent in the environment
+state = env.reset()
+state = np.reshape(state, state_shape)
+done = False
+while not done:
+    action = agent.act(state)
+    next_state, _, done, _ = env.step(action)
+    next_state = np.reshape(next_state, state_shape)
+    # Take action with the agent in the environment
