d3f0fbef7d6e5c9537030603e85669898e683ed9
Ashvin Bhat <ashvinbhat14@gmail.com>
2023-07-23 21:36:32 +0530

    multiple robot nodes working

---
+++ src/scripts/task_scheduler/robot4.py
@@ -0,0 +1,29 @@
+import rospy
+from fredbots.srv import TaskAssign
+
+def robot_client(robot_id, x, y):
+    rospy.wait_for_service('tasker')
+    try:
+        tasker = rospy.ServiceProxy('tasker', TaskAssign)
+        resp = tasker(robot_id, x, y)
+        return resp.robot_id, resp.x, resp.y
+    except rospy.ServiceException as e:
+        print("Service call failed: %s" % e)
+        return None, None, None
+
+if __name__ == "__main__":
+    rospy.init_node("robot4_client")
+
+    robot_id = 4
+    current_x = 9
+    current_y = 3
+
+    while not rospy.is_shutdown():
+        robot_id_result, final_x, final_y = robot_client(robot_id, current_x, current_y)
+
+        if robot_id_result is not None:
+            rospy.loginfo(f"Received response from Robot {robot_id_result}: Final Position: ({final_x}, {final_y})")
+        else:
+            rospy.logerr("Failed to get a valid response.")
+
+        rospy.sleep(1)
---
+++ src/scripts/task_scheduler/tasker_testing.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+
+import time
+from threading import Thread
+import numpy as np
+from tabulate import tabulate
+import multiprocessing as mp
+from fredbots.srv import TaskAssign
+from fredbots.srv import TaskAssignResponse
+from fredbots.srv import TaskAssignRequest
+
+import rospy
+
+class Robot:
+    def __init__(self, robot_id, intital_position, battery_capacity=100):
+        self.robot_id = robot_id
+        self.current_position = intital_position
+        self.batter_level = battery_capacity
+        self.current_task = None
+        self.is_idle = True
+        self.destination_position = None
+
+    def __str__(self):
+        return f'Robot {self.robot_id}'
+
+    def assign_package(self, package):
+        self.current_task = package
+        update_robot_task(self)
+        self.destination_position = package.pickup_position
+
+    def update_position(self, new_position):
+        self.current_position = new_position
+
+    def update_battery(self):
+        if self.is_idle:
+            self.batter_level -= 0.05
+        elif self.current_task.weight > 0:
+            self.batter_level -= 0.1 * self.current_task.weight/10
+        else:
+            self.batter_level -= 0.1
+
+
+class Package:
+    def __init__(self, package_id, pickup_position, dropoff_position, priority, weight=3):
+        self.package_id = package_id
+        self.pickup_position = pickup_position
+        self.dropoff_position = dropoff_position
+        self.weight = weight
+        self.priority = priority
+        self.picked_up = False
+        self.delivered = False
+        self.assigned = False
+        self.utilities = {}
+
+    def __str__(self):
+        return f'Package {self.package_id}'
+
+    def remove(self):
+        self.picked_up = True
+        self.delivered = True
+
+
+def euclidean_distance(coords1, coords2):
+    x1, y1 = coords1
+    x2, y2 = coords2
+    return np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
+
+def assign_tasks(robots, packages):
+    for package in packages:
+        if not package.picked_up:
+            max_distance = max([euclidean_distance(robot.current_position, package.pickup_position) for robot in robots.values()])
+            # store utility for each robot in package utility dictionary and sort by highest utility first  (descending order)
+            package.utilities = {robot.robot_id: calculate_utility(robot, package, max_distance) for robot in robots.values()}
+            package.utilities = {k: v for k, v in sorted(package.utilities.items(), key=lambda item: item[1], reverse=True)}
+    # packages.sort(key=lambda x: get_max_utility(x), reverse=True)
+    # sort packages by first value in utilities dictionary (highest utility)
+    packages.sort(key=lambda x: list(x.utilities.values())[0], reverse=True)
+    assigned_robots = []
+    for i in range(len(packages)):
+        packages[i].utilities = {k: v for k, v in sorted(packages[i].utilities.items(), key=lambda item: item[1], reverse=True)}
+        # packages.sort(key=lambda x: get_max_utility(x), reverse=True)
+        packages.sort(key=lambda x: list(x.utilities.values())[0], reverse=True)
+        for robot_id, utility in packages[i].utilities.items():
+            if robot_id not in assigned_robots:
+                robots[robot_id - 1].assign_package(packages[i])
+                assigned_robots.append(robot_id)
+                packages[i].assigned = True
+                print(f'package {packages[i].package_id} assigned to robot {robot_id}')
+                for other_package in packages:
+                    if other_package.package_id != packages[i].package_id:
+                        other_package.utilities[robot_id] = -1
+                break
+            else:
+                packages[i].utilities[robot_id] = 0
+    # return robots
+
+ 
+def calculate_utility(robot, package, max_distance):
+    distance = euclidean_distance(robot.current_position, package.pickup_position)
+    utilty = (1 - distance/max_distance) * 0.3 + package.priority * 0.7
+    # print(f'Utility of {robot} for package {package.package_id} is {utilty}')
+    return utilty
+
+
+def update_robot_position(robot, new_position):
+    robot.update_position(new_position)
+
+
+def update_robot_battery(robot):
+    robot.update_battery()
+
+
+def update_robot_task(robot):
+    if robot.current_task is not None:
+        if robot.current_task.picked_up and not robot.current_task.delivered:
+            robot.is_idle = False
+            robot.destination_position = robot.current_task.dropoff_position
+        elif not robot.current_task.picked_up:
+            robot.is_idle = False
+            robot.destination_position = robot.current_task.pickup_position
+    else:
+        robot.is_idle = True
+        robot.destination_position = None
+
+def tasker_server():
+    print("done")
+    rospy.init_node('tasker_server')
+    s = rospy.Service('tasker', TaskAssign, handle_tasker)
+    rospy.loginfo("Tasker server ready.")
+    rospy.spin()
+
+def handle_tasker(req):
+    global robots
+    robot_id = req.robot_id
+    x_pos = req.x
+    y_pos = req.y
+
+    rospy.loginfo(f"Received request from robot {robot_id} at position ({x_pos}, {y_pos})")
+    rospy.loginfo("Assigning tasks...")
+
+    robots[robot_id-1] = Robot(robot_id, (x_pos, y_pos))
+
+    assign_tasks(robots, packages)
+
+    return robot_id, x_pos, y_pos
+
+
+
+def main():
+    global robots, packages
+    packages = [
+        Package(1, (2, 2), (8, 8), 8, 5),
+        Package(2, (5, 5), (3, 1), 4, 10),
+        Package(3, (1, 1), (9, 9), 6, 2),
+        Package(4, (7, 7), (4, 6), 2, 1),
+    ]
+
+    # sort packages by priority
+    packages.sort(key=lambda x: x.priority, reverse=True)
+
+    # robots
+    robots = dict()
+    tasker_server() 
+
+if __name__ == '__main__':
+    main()
