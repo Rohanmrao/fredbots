032befba942908d6266320226d3cf1ede6f75038
Amulya Marali <maraliamulya@gmail.com>
2023-07-24 23:52:55 +0530

    created new folder shinchan and added some imp files

---
+++ src/scripts/dqn_only/swalpa_lidar.py
@@ -0,0 +1,22 @@
+import rospy
+from sensor_msgs.msg import LaserScan
+
+def laser_scan_callback(msg):
+    # Access the range measurements from the LaserScan message
+    ranges = msg.ranges
+
+    # Find the minimum and maximum range values
+    min_range = min(ranges)
+    max_range = max(ranges)
+
+    print("mini range: ", min_range)
+
+    
+
+    # Print the results
+    # rospy.loginfo("Minimum range: %.2f meters" % min_range)
+    # rospy.loginfo("Maximum range: %.2f meters" % max_range)
+
+rospy.init_node('laser_scan_subscriber')
+rospy.Subscriber('/scan', LaserScan, laser_scan_callback)
+rospy.spin()
--- src/scripts/new/frozen_lake/diff_target/space_gamer_dnd/test.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-a = np.array([5,2,35,1])
-print(np.argsort(a)[-3])
