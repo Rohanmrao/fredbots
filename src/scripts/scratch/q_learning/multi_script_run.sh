438476e6f97c2dc027264265046c300b7ca115f2
Rohanmrao <rohanmaheshrao10@gmail.com>
2023-07-25 15:45:46 +0530

    comments

--- src/scripts/scratch/q_learning/multi_script_run.sh
+++ src/scripts/scratch/q_learning/multi_script_run.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/bash
 
+# this runs the q learning scripts in parallel
+
 python3 /home/rohan/catkin_ws/src/fredbots/src/scripts/scratch/q_learning/ql_bot_1.py &
 python3 /home/rohan/catkin_ws/src/fredbots/src/scripts/scratch/q_learning/ql_bot_2.py &
 python3 /home/rohan/catkin_ws/src/fredbots/src/scripts/scratch/q_learning/ql_bot_3.py &
