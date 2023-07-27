e05ab069d9a803bf47ada7b7a2f0c3c8f087f0d0
Amulya Marali <maraliamulya@gmail.com>
2023-07-27 23:19:34 +0530

    changed file system after merging

--- src/scripts/task_scheduler/robot3.py
+++ src/scripts/task_scheduler/robot3.py
@@ -6,10 +6,10 @@ def robot_client(robot_id, x, y):
     try:
         tasker = rospy.ServiceProxy('tasker', TaskAssign)
         resp = tasker(robot_id, x, y)
-        return resp.robot_id, resp.x, resp.y
+        return resp.robot_id, resp.a, resp.b, resp.x, resp.y 
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
-        return None, None, None
+        return None, None, None, None, None
 
 if __name__ == "__main__":
     rospy.init_node("robot3_client")
@@ -19,10 +19,11 @@ if __name__ == "__main__":
     current_y = 8
 
     while not rospy.is_shutdown():
-        robot_id_result, final_x, final_y = robot_client(robot_id, current_x, current_y)
+        robot_id_result, pickup_x, pickup_y, destination_x, destination_y = robot_client(robot_id, current_x, current_y)
+        print("id, pickup_x, pickup_y: ", robot_id_result, pickup_x, pickup_y, destination_x, destination_y)
 
         if robot_id_result is not None:
-            rospy.loginfo(f"Received response from Robot {robot_id_result}: Final Position: ({final_x}, {final_y})")
+            rospy.loginfo(f"Received response from Robot {robot_id_result}: Pickup Position: ({pickup_x}, {pickup_y}), Destination Position: ({destination_x}, {destination_y})")
         else:
             rospy.logerr("Failed to get a valid response.")
 
