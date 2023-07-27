4441d6806241092302b807eae47e33592940db06
Rohanmrao <rohanmaheshrao10@gmail.com>
2023-07-27 12:10:39 +0530

    pos changes

--- src/scripts/scratch/q_learning/multi_script_run.sh
+++ src/scripts/scratch/q_learning/multi_script_run.sh
@@ -2,8 +2,8 @@
 
 # this runs the q learning scripts in parallel
 
-python3 /home/rohan/catkin_ws/src/fredbots/src/scripts/scratch/q_learning/ql_bot_1.py &
-python3 /home/rohan/catkin_ws/src/fredbots/src/scripts/scratch/q_learning/ql_bot_2.py &
-python3 /home/rohan/catkin_ws/src/fredbots/src/scripts/scratch/q_learning/ql_bot_3.py &
-python3 /home/rohan/catkin_ws/src/fredbots/src/scripts/scratch/q_learning/ql_bot_4.py &
+python3 /home/rohan/catkin_ws/src/fredbots/src/scripts/scratch/q_learning/ql_bot_1.py
+#python3 /home/rohan/catkin_ws/src/fredbots/src/scripts/scratch/q_learning/ql_bot_2.py &
+#python3 /home/rohan/catkin_ws/src/fredbots/src/scripts/scratch/q_learning/ql_bot_3.py &
+#python3 /home/rohan/catkin_ws/src/fredbots/src/scripts/scratch/q_learning/ql_bot_4.py &
 
